d90c65d0b1657c1662c08f3046dd3da1c10151e7
stevethepug <adam.a.french@btinternet.com>
2022-11-19 01:01:41 +0000

    Converted vanish to BaseCommand

--- src/main/java/me/stevethepug/edwardplugin/cmd/Vanish.java
+++ src/main/java/me/stevethepug/edwardplugin/cmd/Vanish.java
@@ -2,61 +2,44 @@ package me.stevethepug.edwardplugin.cmd;
 
 import me.stevethepug.edwardplugin.EdwardPlugin;
 import org.bukkit.Bukkit;
-import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
-public class Vanish implements CommandExecutor {
-    EdwardPlugin plugin;
+import java.util.ArrayList;
 
-    public Vanish(EdwardPlugin instance) {plugin = instance;}
-    // This method is called, when somebody uses our command
+public class Vanish extends BaseCommand {
+    //Array of players using the vanishing command
+    ArrayList<Player> playersVanishing;
+    EdwardPlugin plugin;
+    public Vanish(EdwardPlugin instance) {plugin = instance; playersVanishing = new ArrayList<>();}
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-    {
-        if (cmd.getName().equalsIgnoreCase("vanish"))
-        {
-            if (sender instanceof Player)
-            {
-                Player player = (Player) sender;
-                if (player.isOp())
-                {
-                    if (args.length == 0)
-                    {
-                        VanishPlayer(player);
-                    }
-                    if (args.length == 1)
-                    {
-                        String s = args[0];
-                        if (Bukkit.getServer().getPlayer(s) != null && Bukkit.getServer().getPlayer(s).getName().equalsIgnoreCase(s))
-                        {
-                            Player p = Bukkit.getPlayer(s);
-                            VanishPlayer(p);
-                        } else
-                        {
-                            player.sendMessage("Error: Invalid target");
-                        }
-                    } else
-                    {
-                        player.sendMessage("Error: Invalid input");
-                    }
-                }
-            }
-        }
-        return true;
+    public String GetName() {
+        return "vanish";
     }
-    public void VanishPlayer(Player player)
-    {
-        if (plugin.vanishedPlayers.contains(player))
+
+    @Override
+    public String GetUsage() {
+        return "/vanish <user>";
+    }
+
+    @Override
+    public void ExecuteCommand(Player player, String[] args) {
+        if (playersVanishing.contains(player))
         {
-            plugin.vanishedPlayers.remove(player);
-            player.showPlayer(plugin, player);
-            player.sendMessage("boo nig");
+            player.sendMessage("Showing other players");
+            playersVanishing.remove(player);
+            for (Player otherPlayer : Bukkit.getOnlinePlayers())
+            {
+                player.showPlayer(plugin, otherPlayer);
+            }
         }
         else
         {
-            plugin.vanishedPlayers.add(player);
-            player.hidePlayer(plugin, player);
-            player.sendMessage("raahted man gone");
+            player.sendMessage("Hiding other players");
+            playersVanishing.add(player);
+            for (Player otherPlayer : Bukkit.getOnlinePlayers())
+            {
+                player.hidePlayer(plugin, otherPlayer);
+            }
         }
     }
 }
