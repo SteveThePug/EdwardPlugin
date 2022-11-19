69013681a334a3047bc4a26737399176da7f557e
stevethepug <adam.a.french@btinternet.com>
2022-11-19 15:38:26 +0000

    Plugin commonly needed for all commands, now inherited

--- src/main/java/me/stevethepug/edwardplugin/cmd/BaseCommand.java
+++ src/main/java/me/stevethepug/edwardplugin/cmd/BaseCommand.java
@@ -1,9 +1,12 @@
 package me.stevethepug.edwardplugin.cmd;
 
+import me.stevethepug.edwardplugin.EdwardPlugin;
 import org.bukkit.entity.Player;
 
 public abstract class BaseCommand
 {
+    public EdwardPlugin plugin;
+    public BaseCommand(EdwardPlugin instance) {this.plugin = instance;}
     public abstract String GetName();
 
     public abstract String GetUsage();
